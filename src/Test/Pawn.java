7ae8f3bd465db4fd0a3241d5c93570975134922f
reaperes <emerald105@hanmail.net>
2013-07-17 01:54:07 +0900

    ~chap.2-2

--- src/Test/Pawn.java
+++ src/Test/Pawn.java
@@ -1,7 +1,14 @@
 package Test;
 
 public class Pawn {
+	final String PAWN_WHITE = "white";
+	final String PAWN_BLACK = "black";
+	
 	private String color;
+	
+	public Pawn() {
+		this.color = "white";
+	}
 
 	public void setColor(String color) {
 		this.color = color;
--- test/Test/PawnTest.java
+++ test/Test/PawnTest.java
@@ -4,15 +4,12 @@ import junit.framework.TestCase;
 
 public class PawnTest extends TestCase {
 	public void testCreate(){
-		final String white = "white";
-		final String black = "black";
-		
 		Pawn pawn1 = new Pawn();
-		pawn1.setColor(white);
-		assertEquals(white, pawn1.getColor());
+//		pawn1.setColor(white);
+		assertEquals(pawn1.PAWN_WHITE, pawn1.getColor());
 		
 		Pawn pawn2 = new Pawn();
-		pawn2.setColor(black);
-		assertEquals(black, pawn2.getColor());
+//		pawn2.setColor(black);
+		assertEquals(pawn2.PAWN_BLACK, pawn2.getColor());
 	}
 }
